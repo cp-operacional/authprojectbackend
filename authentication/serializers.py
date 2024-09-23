461c3a0df22477e47b6148e05c9497408e93489c
cp-operacional <operacional@contactpro.com.br>
2024-09-23 11:06:22 -0300

    feat: updating avatar image in serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -11,15 +11,21 @@ class UserAccountSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        if 'avatar' not in validated_data or not validated_data['avatar']:
+            validated_data.pop('avatar', None)
         user = UserAccount.objects.create_user(**validated_data)
         return user
 
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
+        avatar = validated_data.pop('avatar', None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         if password:
             instance.set_password(password)
+        if avatar:
+            instance.avatar = avatar
+        elif 'avatar' in self.initial_data:
+            instance.avatar = None
         instance.save()
         return instance
-      
