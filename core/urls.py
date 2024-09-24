56034d2b1e809c446ea1300fc8e009afef89d23e
cp-operacional <operacional@contactpro.com.br>
2024-09-24 12:14:45 -0300

    feat: static and template files setup

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework import routers
@@ -25,3 +26,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
